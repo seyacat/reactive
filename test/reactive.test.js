b4f15fed3990a86952e5c1d6b456b377a170cad6
Seyacat <seyacat@gmail.com>
2023-12-19 18:20:41 -0500

    test2

--- test/reactive.test.js
+++ test/reactive.test.js
@@ -4,6 +4,7 @@ const assert = require("assert");
 
 it("Reactive Basic", function () {
   const games = Reactive();
+  //SUBSCRIBE to test property changes
   games.subscribe("test", (data) => {
     const { base, prop, path, pathValues, value, oldValue } = data;
     assert.equal(prop, "test");
@@ -13,4 +14,26 @@ it("Reactive Basic", function () {
     assert.equal(oldValue, undefined);
   });
   games.test = 1;
+});
+
+it("Reactive Chain", function () {
+  const games = Reactive(
+    {
+      level1: Reactive([Reactive({ level3: "OK" }, { prefix: "level2" })], {
+        prefix: "level1",
+      }),
+    },
+    { prefix: "base" }
+  );
+  //SUBSCRIBE to every change in Reactive chain
+  games.subscribe(null, (data) => {
+    const { base, prop, path, pathValues, value, oldValue } = data;
+    console.log("DATA", { data });
+    assert.equal(prop, "level3");
+    assert.equal(JSON.stringify(path), JSON.stringify(["level1", 0, "level3"]));
+    assert.equal(value, "KO");
+    assert.equal(oldValue, "OK");
+  });
+  assert.equal(games.level1[0].level3, "OK");
+  games.level1[0].level3 = "KO";
 });
